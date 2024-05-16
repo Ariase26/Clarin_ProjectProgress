95948de2266e602a4969b785d2656dc826de323a
LAB11_USER ON WS28 <L11Y08W28@citulab.net>
2024-05-16 10:19:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/clarin_projectprogress/ImmediateNeedForm.java
@@ -0,0 +1,13 @@
+package com.example.clarin_projectprogress;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImmediateNeedForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immediate_need_form);
+    }
+}
